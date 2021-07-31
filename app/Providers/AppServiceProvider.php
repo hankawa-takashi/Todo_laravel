b78f9f36f6b7b2ebc0201c6a4f809473f906b45e
hankawa-takashi <w7dwca7288s7h7q@docomo.ne.jp>
2021-07-31 13:01:56 +0900

    add defaultstring

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+    use Illuminate\Support\Facades\Schema;
 
     /**
      * Bootstrap any application services.
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         //
     }
+
 }
